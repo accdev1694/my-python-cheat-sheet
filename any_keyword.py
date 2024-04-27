edde4656626dcbf08c8478fa9be8a6e59b908766
accdev1694 <164797828+accdev1694@users.noreply.github.com>
2024-04-27 12:40:00 +0100

    typo correction

--- any_keyword.py
+++ any_keyword.py
@@ -1,5 +1,6 @@
-# use the any keyword to iterate over a list or any iterable to chaech if any of the characters meet a certain condition
+# use the any keyword to iterate over a list or any iterable to check if any of the characters meet a certain condition
 # in this case if any of the characters is a number or digit
+
 number = input("Plate: ")
 if any(num.isdigit() for num in number):
     print("There are Digits!")
