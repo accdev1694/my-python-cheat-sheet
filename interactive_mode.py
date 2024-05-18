10d870973ad73d4d993b84ad46474139d8a8301f
unknown <diloch111@gmail.com>
2024-05-18 05:56:14 +0100

    interactive mode

---
+++ interactive_mode.py
@@ -0,0 +1,6 @@
+# you can write code firectly inside your terminal and see your results immediately without ussing the enter key to run
+
+# type python in the terminal and you see the >>> icon, then start typing in code
+# 1 + 1, etc
+
+# once you are done, type exit() to go back to terminal mode
