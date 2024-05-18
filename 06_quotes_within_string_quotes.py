c9a450452c276dcfa9d119e6619b14f86744fdf1
unknown <diloch111@gmail.com>
2024-05-18 05:34:17 +0100

    using quotes within quotes

---
+++ 06_quotes_within_string_quotes.py
@@ -0,0 +1,9 @@
+# you can specify quotation marks within a the quotes in a string of text by using two methods
+
+# 1.) use another variant of quotes
+print('hello, "friend"') # or
+print("hello, 'friend'")
+
+# 02.) use an escape character
+print('hello, \'guys\'')
+# this tells python interpreter that the use of the inner quotes is to be taken literally and get it printed
