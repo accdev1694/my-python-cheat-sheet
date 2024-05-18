e110adff05dc2e6d0b4d46b721ff1a2e44de48b6
unknown <diloch111@gmail.com>
2024-05-18 04:53:37 +0100

    comments

---
+++ comments.py
@@ -0,0 +1,2 @@
+# use the pound sign to write comments, preceed each line with the pound sign
+# use three quotes at the start and end of a multi line comment
---
+++ line_end_new_line_seperators.py
@@ -0,0 +1,3 @@
+# \n is an escape character which is set to True by defaultin the print command
+# whenever you use multiple aguments with the print command, sep=" " by default. 
+print("Hello,","Oloche")
