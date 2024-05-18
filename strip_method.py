cd9943bd311051280fa3f2cb0966d21ba024780d
unknown <diloch111@gmail.com>
2024-05-18 05:42:10 +0100

    strip method for strings

---
+++ strip_method.py
@@ -0,0 +1,9 @@
+# the strip method
+
+# this is used on strings fo remove white spaces from the begining and end of a string
+name = input("what is your name? ")
+name = name.strip()
+#you can reassign values of variables to the same name
+
+# you can combine like this
+age = input("Age: ").strip()
