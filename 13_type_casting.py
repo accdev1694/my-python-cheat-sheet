19cfffaf07723b2589faa47445415b8b64d45b2e
unknown <diloch111@gmail.com>
2024-05-18 06:05:44 +0100

    type casting example

---
+++ 13_type_casting.py
@@ -0,0 +1,9 @@
+# convert one data type to another
+
+# the return value of the input function is a string
+
+# convert that str to an int in order to perform arithmetics on them
+x = int(input("Whats x: "))
+y = int(input("whats y: "))
+z = x + y
+print(z)
