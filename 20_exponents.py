6ed5e1c307b816e8281b20df1ab86b79e04219fa
unknown <diloch111@gmail.com>
2024-05-18 08:04:49 +0100

    exponentiations

---
+++ 20_exponents.py
@@ -0,0 +1,10 @@
+# two ways to raise a number to a power
+
+# 1.) pow function
+pow_function = pow(2,4)
+print(pow_function, '(using pow function)')
+
+# 2.) exponents operator
+exponentiation = 2**5
+print(exponentiation, '(using exponents)')
+
