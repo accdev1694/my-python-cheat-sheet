56c46c24f894eacd703d1d90440988682629b9fe
unknown <diloch111@gmail.com>
2024-05-19 03:50:46 +0100

    multiply strings

---
+++ 28_multiply_strings.py
@@ -0,0 +1,5 @@
+# it turns out you can mutiply a string by a number
+
+print("Hello\n" *3, end="")
+# i have used the newline escape character to seperate each print into a new line
+# i have also removed the default new line at the end
