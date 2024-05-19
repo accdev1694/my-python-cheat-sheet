122792b5757463549e7870e461a1e5822924baa2
unknown <diloch111@gmail.com>
2024-05-19 02:09:30 +0100

    if and logical expressions

---
+++ 22_if_and_logical_expressions.py
@@ -0,0 +1,7 @@
+# we can combine or statements inside if statements, this shortens our code
+age = int(input("How old are you? "))
+
+if age >= 13 and age <= 19:
+    print("You are a tenager")
+else:
+    print("You are not a teenager")
