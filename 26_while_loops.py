f1bf17e8512afe6565d59f695d3d4216c1391005
unknown <diloch111@gmail.com>
2024-05-19 03:24:04 +0100

    while loops

---
+++ 26_while_loops.py
@@ -0,0 +1,10 @@
+# while loops are used to repead some code over and over until you break out of the code
+# lets print hello 5 times
+count = 0
+while count < 5:
+    print("Hello")
+    count += 1
+    
+# while count is less than 5 you keep printing hello
+# then add 1 to count
+# the momment count becomes more than 5 you break out of the loop
