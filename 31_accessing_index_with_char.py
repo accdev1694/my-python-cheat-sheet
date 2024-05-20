4fed5f11bd54ad9cfb534cc64bdc75ffb5044e9c
unknown <diloch111@gmail.com>
2024-05-20 17:24:19 +0100

    accessing index and characters in an iterable

---
+++ 31_accessing_index_with_char.py
@@ -0,0 +1,7 @@
+# you can make a numbered list of items in python
+
+days = ["Sunday", "Monday", "Tuesday",
+        "Wednesday", "Thursday", "Friday", "Saturday"]
+
+for day in range(len(days)):
+    print(day+1, days[day])
---
+++ practice.py
@@ -0,0 +1,7 @@
+day = input("Day: ")
+
+match day:
+    case "1":
+        print("Monday")
+    case _:
+        print("no day")
