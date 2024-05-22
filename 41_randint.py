d21c3a38fa18be726047185af0a067ad1d9fde95
unknown <diloch111@gmail.com>
2024-05-22 20:21:40 +0100

    the randint function

---
+++ 41_randint.py
@@ -0,0 +1,7 @@
+import random
+
+# when you use the randint function, it returns a list of integers
+# this function takes two parameters, a range, the end number inclusive
+
+num = random.randint(0, 2)
+print(num)
