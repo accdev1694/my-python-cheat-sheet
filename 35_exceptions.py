48764f82d9fa7b9f617f475ca73f7e7b3af4ceed
unknown <diloch111@gmail.com>
2024-05-22 18:15:39 +0100

    while loops, try and except

---
+++ 35_exceptions.py
@@ -0,0 +1,17 @@
+# exceptions arent really good things in code. they mean something unexpected has happened that has the capacity to break your code
+
+# let us create a program to collect a number from a  user and print it to the terminal
+
+def main():
+    age = get_age()
+    print(f"You will be {age + 1} years old next year")
+    
+def get_age():
+    while True:
+        user_age = input("What is your age: ")
+        try:
+            return int(user_age)
+        except ValueError:
+            print("You must type a number")        
+
+main()
