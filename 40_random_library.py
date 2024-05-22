cccece9e8f4d13b25a93a5d0d56e51558cd5f5d3
unknown <diloch111@gmail.com>
2024-05-22 20:18:20 +0100

    random module and choice function

---
+++ 40_random_library.py
@@ -0,0 +1,18 @@
+import random
+ # you may also use the from keyword to import just the choice function from the random module
+ # from random import choice
+ # in which case you dont need the dot notation
+ # choices = choice(["Head", "Tail"]) 
+
+# the random.py file is inbuilt with with python and i can be imported to be used in your code
+# note, this choice function only works on lists
+# lets flip a coin
+
+choices = random.choice(["Tail", "Head"])
+print(choices)
+
+# the shuffle module shuffles the list in question, in place
+
+print(random.shuffle(["spade", "heart", "queen"]))
+
+
