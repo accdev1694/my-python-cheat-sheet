7a6c92f065e80763fec1016335b594feced89aab
unknown <diloch111@gmail.com>
2024-05-27 02:28:43 +0100

    calculator program for unit testing

---
+++ p50_pytest.py
@@ -0,0 +1,18 @@
+# because manual testing casn be so verbose, we use a module pytest
+# write code to test your code
+# principally, write your test first
+# lets explore or calculator program
+
+def main():
+    x = int(input("What's x? "))
+    print("x is", square(x))
+    
+    
+def square(n):
+    return n * n
+
+
+if __name__ == "__main__":
+    main()
+    
+# we create a test file for this calculator naming it test_p50_pytest.py
--- practice.py
+++ practice.py
@@ -1,13 +1,29 @@
-import sys
-import requests
-import json
+# Print the name and price of each product.
+# Calculate the total value of each product (price * quantity).
+# Print the total value of each product.
 
+store_products = {
+    'product1': {
+        'name': 'Laptop',
+        'price': 1200,
+        'quantity': 5
+    },
+    'product2': {
+        'name': 'Smartphone',
+        'price': 800,
+        'quantity': 10
+    },
+    'product3': {
+        'name': 'Tablet',
+        'price': 400,
+        'quantity': 8
+    }
+}
 
-response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json').json()
-
-#print(response['bpi']['USD']['rate'])
-print(json.dumps(response, indent=2))
-    
+for name, details in store_products.items():
+    print(f"{details['name']}: {details['price']}")
+    print(f"Total Value = {details['price'] * details['quantity']}")
+    print()
 
 
 
