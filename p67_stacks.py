211cc61e60afe2fe44eacb4357d825409f4fa091
unknown <diloch111@gmail.com>
2024-06-24 15:45:14 +0100

    stacks explained

---
+++ p67_stacks.py
@@ -0,0 +1,5 @@
+#  the principle of stacks in programming is based on the LIFO last in first out concept
+# this means the last item added is the first item removed
+# this is wha browsers use to return you to the previous page once you click on the back button
+# and if the stack is exhausted, the back button is disabled
+
