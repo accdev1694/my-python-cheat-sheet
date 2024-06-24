11992a40dc417780145bfe955fc9a48005f939fd
unknown <diloch111@gmail.com>
2024-06-24 17:10:13 +0100

    more on list comprehensions

--- p74_more_on_list_comprehensions.py
+++ p74_more_on_list_comprehensions.py
@@ -1,3 +1,5 @@
+# list comprehensions are very pythonic. heres another example:
+
 values = []
 for x in range(5):
     values.append(x * 2)
@@ -5,5 +7,6 @@ print(values)
 
 print()
 
+# for the above code, heres the equivalent list comprehension
 values = [char*2 for char in range(5)]
 print(values)
